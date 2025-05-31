c77b832cbb2b1a9d948a78c48fa92a9e8d11c2eb
GiraffeCode <mitcho426@users.noreply.github.com>
2025-05-31 13:31:38 -0700

    Initial Commit

---
+++ GJCalorieTrackerApp/GJCalorieTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GJCalorieTrackerAppApp.swift
+//  GJCalorieTrackerApp
+//
+//  Created by Mitchell Wong on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GJCalorieTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GJCalorieTrackerAppTests/GJCalorieTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GJCalorieTrackerAppTests.swift
+//  GJCalorieTrackerAppTests
+//
+//  Created by Mitchell Wong on 5/31/25.
+//
+
+import Testing
+@testable import GJCalorieTrackerApp
+
+struct GJCalorieTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
